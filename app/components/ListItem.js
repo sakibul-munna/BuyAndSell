5281999b9228f222c350bfc361233dfa49812843
Sakibul_Munna <sakibul.munnaa@gmail.com>
2021-06-15 13:38:58 +0600

    Updated ListItem Component to make it more reusable.

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -10,15 +10,23 @@ import colors from "../config/colors";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import AppText from "./AppText";
 
-function ListItem({ title, subtitle, image, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subtitle,
+  image,
+  ImageComponent,
+  onPress,
+  renderRightActions,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image style={styles.image} source={image} />
-          <View>
+          {ImageComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subtitle}>{subtitle}</AppText>
+            {subtitle && <AppText style={styles.subtitle}>{subtitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -30,6 +38,10 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   image: {
     height: 70,

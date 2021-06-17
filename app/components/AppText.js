a28cd807b87ec76ccdaf34ada6b79b8b3c8d481b
Sakibul_Munna <sakibul.munnaa@gmail.com>
2021-06-18 01:58:43 +0600

    AppText Component updated using the new styles.

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,25 +1,10 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
-import colors from "../config/colors";
+import { Text } from "react-native";
+
+import defaultStyles from "../config/styles";
 
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: colors.black,
-    ...Platform.select({
-      ios: {
-        fontSize: 12,
-        fontFamily: "avenir",
-      },
-      android: {
-        fontSize: 18,
-        fontFamily: "Roboto",
-      },
-    }),
-  },
-});
 
 export default AppText;

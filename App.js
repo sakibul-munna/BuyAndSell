c1a28e63f2e8a1289a3e3094138f76bd544a02a2
Sakibul_Munna <sakibul.munnaa@gmail.com>
2021-06-23 16:48:40 +0600

    Custom Picker Component Created!

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
+import AppCard from "./app/components/AppCard";
+import AppPicker from "./app/components/AppPicker";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import ListingScreen from "./app/screens/ListingScreen";
+import AccountScreen from "./app/screens/AccountScreen";
+import AppTextInput from "./app/components/AppTextInput";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  return <WelcomeScreen />;
+  const [category, setCategory] = useState(categories[0]);
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }

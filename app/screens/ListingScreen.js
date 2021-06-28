dbd31469464a276d38717ba01399652fbcf54304
Sakibul_Munna <sakibul.munnaa@gmail.com>
2021-06-28 21:00:34 +0600

    Back-end/API errors have been handled.

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, Button } from "react-native";
 
-import Screen from "../components/Screen";
+import AppButton from "../components/AppButton";
 import AppCard from "../components/AppCard";
+import AppText from "../components/AppText";
 import colors from "../config/colors";
 import getListings from "../api/listings";
 import routes from "../navigations/routes";
+import Screen from "../components/Screen";
 
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   const loadListings = async () => {
     const response = await getListings();
-    setListings(response.data);
+    if (!response.ok) {
+      return setError(true);
+    } else {
+      setError(false);
+      setListings(response.data);
+    }
   };
 
   useEffect(() => {
@@ -22,6 +30,12 @@ function ListingsScreen({ navigation }) {
   useEffect;
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings!</AppText>
+          <AppButton title="Retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
